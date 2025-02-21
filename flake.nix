abb59e16b0dbe11cae9bc02dac9be151556a3034
James Chubbuck <80338799+jameschubbuck@users.noreply.github.com>
2025-02-21 20:55:14 +0000

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -1,41 +1,45 @@
 {
-  inputs = { 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    stylix = {
-      url = "github:danth/stylix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nvf = {
-      url = "github:notashelf/nvf";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    stylix.url = "github:danth/stylix";
+    nixos-apple-silicon.url = "github:tpwrules/nixos-apple-silicon";
   };
-  outputs = { self, nixpkgs, home-manager, stylix, nvf, ... }@inputs: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    configModule = import ./modules/nvf-modules/config.nix;
-    customNeovim = nvf.lib.neovimConfiguration { inherit pkgs; modules = [configModule];};
+  outputs = { self, nixpkgs, home-manager, stylix, nixos-apple-silicon, ...}:
+  let 
+    overlays = [
+      self.overlays.default
+    ];
   in {
-    packages.${system}.my-neovim = customNeovim.neovim; 
+    overlays.default = final: prev: {
+      libkrun = final.callPackage ./modules/nix-modules/emulation/libkrun { };
+      muvm = final.callPackage ./modules/nix-modules/emulation/muvm { };
+      virglrenderer = prev.virglrenderer.overrideAttrs (old: {
+        src = final.fetchurl {
+          url = "https://gitlab.freedesktop.org/asahi/virglrenderer/-/archive/asahi-20241205.2/virglrenderer-asahi-20241205.2.tar.bz2";
+          hash = "sha256-mESFaB//RThS5Uts8dCRExfxT5DQ+QQgTDWBoQppU7U=";
+        };
+        mesonFlags = old.mesonFlags ++ [ (final.lib.mesonOption "drm-renderers" "asahi-experimental") ];
+      });
+    };
     nixosConfigurations = {
-      "james" = nixpkgs.lib.nixosSystem {
+      "mba" = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
         modules = [
-	  {environment.systemPackages = [customNeovim.neovim];}
-          ./modules/nix-modules/config.nix
-          stylix.nixosModules.stylix
-          home-manager.nixosModules.home-manager
           {
-            home-manager.useGlobalPkgs = true;
+            nixpkgs.overlays = overlays;
+          }
+          ./modules/nix-modules/configuration.nix
+          stylix.nixosModules.stylix
+          nixos-apple-silicon.nixosModules.default
+          home-manager.nixosModules.home-manager { 
+            home-manager.useGlobalPkgs = false;
             home-manager.useUserPackages = true;
             home-manager.users.james = import ./modules/hm-modules/configuration.nix;
-            home-manager.extraSpecialArgs = {inherit inputs; };
+            #home-manager.extraSpecialArgs = { inherit inputs; };
           }
         ];
       };
-    };
+    }; 
   };
 }
